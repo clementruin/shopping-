be039e1104005ad5b0546b2256401fba0eb4bec4
clementruin <clement.ruin@yahoo.fr>
2017-10-05 00:50:31 +0200

    csv file for the base created

--- base_transaction.py
+++ base_transaction.py
@@ -1,6 +1,7 @@
-# Transaction 
+# Transaction
 
 import sqlalchemy
+import csv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -9,39 +10,45 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
 
+
 class TransactionTable(Base):
-	__tablename__ = 'transactions'
-	id = Column(Integer, primary_key=True)
-	customer = Column(String)
-	shop = Column(String)
-	c_items = Column(Integer)
-	m_size = Column(Integer)
-	amount = Column(Integer)
+    __tablename__ = 'transactions'
+    id = Column(Integer, primary_key=True)
+    customer = Column(String)
+    shop = Column(String)
+    c_items = Column(Integer)
+    m_size = Column(Integer)
+    amount = Column(Integer)
 
-	def __repr__(self):
-		return "<TransactionTable(customer='%s', shop='%s', c_items='%s', m_size='%s', amount='%s')>" % (
-			self.customer, self.shop, self.c_items, self.m_size, self.amount)
+    def __repr__(self):
+        return "<TransactionTable(customer='%s', shop='%s', c_items='%s', m_size='%s', amount='%s')>" % (
+            self.customer, self.shop, self.c_items, self.m_size, self.amount)
 
 
-<<<<<<< HEAD
-#new_transaction = TransactionTable(customer='Kyl', shop='HappyTapis', c_items=3, m_size=12, amount=60)
->>>>>>> 662481e64503b31b58edc8cda309d1adf38fbeef
+new_transaction = TransactionTable(
+    customer='Kyl',
+    shop='HappyTapis',
+    c_items=3,
+    m_size=12,
+    amount=60)
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-session.add(new_request)
-<<<<<<< HEAD
-#session.commit()
+session.add(new_transaction)
+# session.commit()
 
-#for instance in session.query(Request).order_by(Request.id):
-#	print("\n", instance.id, instance.title, instance.description)
-=======
+for instance in session.query(TransactionTable).order_by(TransactionTable.id):
+    print("\n", instance.shop)
+
+outfile = open('mydump.csv', 'w')
+outcsv = csv.writer(outfile)
+records = session.query(TransactionTable).all()
+[outcsv.writerow([getattr(curr, column.name)
+                  for column in TransactionTable.__mapper__.columns]) for curr in records]
+
+outfile.close()
+
 #session.add_all([Request(title=title, description=description)])
-#session.commit()
-
->>>>>>> 662481e64503b31b58edc8cda309d1adf38fbeef
-
-
-
+session.commit()
