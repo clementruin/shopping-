62142cd97c26e2f239227e332172ca37b60f6354
clementruin <clement.ruin@yahoo.fr>
2017-10-05 22:23:23 +0200

    new file with db display on flask server

--- base_transaction.py
+++ base_transaction.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
-
+Base.metadata.create_all(engine)
 
 class TransactionTable(Base):
     __tablename__ = 'transactions'
@@ -24,19 +24,24 @@ class TransactionTable(Base):
         return "<TransactionTable(customer='%s', shop='%s', c_items='%s', m_size='%s', amount='%s')>" % (
             self.customer, self.shop, self.c_items, self.m_size, self.amount)
 
-
 new_transaction = TransactionTable(
     customer='Kyl',
     shop='HappyTapis',
     c_items=3,
     m_size=12,
     amount=60)
+new_transaction2 = TransactionTable(
+    customer='Ive',
+    shop='HappyTapis',
+    c_items=3,
+    m_size=12,
+    amount=60)
 
-Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 session.add(new_transaction)
+session.add(new_transaction2)
 # session.commit()
 
 for instance in session.query(TransactionTable).order_by(TransactionTable.id):
@@ -47,7 +52,7 @@ outcsv = csv.writer(outfile)
 records = session.query(TransactionTable).all()
 [outcsv.writerow([getattr(curr, column.name)
                   for column in TransactionTable.__mapper__.columns]) for curr in records]
-
+print(Base.metadata.tables.keys())
 outfile.close()
 
 #session.add_all([Request(title=title, description=description)])
